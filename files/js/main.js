66fd1c39ffb5317cc79a6172bfa7f69958b670f3
MarkOsBab <m.g4089188@gmail.com>
2022-04-03 22:07:40 -0300

    Funcion de dark / light theme añadida

--- files/js/main.js
+++ files/js/main.js
@@ -22,3 +22,28 @@ $(document).ready(function() {
       $(this).parent().find(".works__box__item__desc").slideToggle();
   });
 });
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.body.setAttribute('class', 'theme--dark');
+        localStorage.setItem('class', 'theme--dark'); //add this
+    }
+    else {
+        document.body.setAttribute('class', 'theme');
+        localStorage.setItem('class', 'theme'); //add this
+    }    
+}
+const currentTheme = localStorage.getItem('class') ? localStorage.getItem('class') : null;
+
+if (currentTheme) {
+    document.body.setAttribute('class', currentTheme);
+
+    if (currentTheme === 'theme--dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+
+toggleSwitch.addEventListener('change', switchTheme, false);
