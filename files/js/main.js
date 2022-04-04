39b68990474e409634f9dc01de1a1a31f1ef7f7f
MarkOsBab <m.g4089188@gmail.com>
2022-04-03 21:44:58 -0300

    Estructura principal y diseño terminado

--- files/js/main.js
+++ files/js/main.js
@@ -14,27 +14,11 @@ function clickHandler(e) {
     behavior: "smooth"
   });
 }
-(function(){
-  emailjs.init("i5G8FFUfFZK0wW36F");
-})();
-$('#app').on('submit', function(event) {
-event.preventDefault(); // prevent reload
 
-var formData = new FormData(this);
-formData.append('service_id', 'service_7s0c5ic');
-formData.append('template_id', 'template_a77qy4l');
-formData.append('user_id', 'i5G8FFUfFZK0wW36F');
-function onSubmit(token) {
-  document.getElementById("app").submit();
-    $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
-    type: 'POST',
-    data: formData,
-    contentType: false, // auto-detection
-    processData: false // no need to parse formData to string
-  }).done(function() {
-    alert('Your mail is sent!');
-  }).fail(function(error) {
-    alert('Oops... ' + JSON.stringify(error));
+$(document).ready(function() {
+  $(".works__box__item__desc").hide();
+  $(".works__box__item__btn").show();
+  $('.works__box__item__btn').click(function() {
+      $(this).parent().find(".works__box__item__desc").slideToggle();
   });
-  }
 });
