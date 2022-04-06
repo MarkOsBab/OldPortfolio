2ba7eae80b0b40054b641921d1be5ee4dc9e3f74
MarkOsBab <m.g4089188@gmail.com>
2022-04-05 21:29:23 -0300

    Dark / light theme terminado

--- files/js/main.js
+++ files/js/main.js
@@ -23,25 +23,31 @@ $(document).ready(function() {
   });
 });
 
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const toggleSwitch = document.querySelector('.navbar__item__switch__label input[type="checkbox"]');
 
 function switchTheme(e) {
     if (e.target.checked) {
         document.body.setAttribute('class', 'theme--dark');
+        document.getElementById('switch-icon').setAttribute('class', 'fa-solid fa-moon');
         localStorage.setItem('class', 'theme--dark'); //add this
+        localStorage.setItem('icon', 'fa-solid fa-moon')
     }
     else {
         document.body.setAttribute('class', 'theme');
+        document.getElementById('switch-icon').setAttribute('class', 'fa-solid fa-sun');
         localStorage.setItem('class', 'theme'); //add this
+        localStorage.setItem('icon', 'fa-solid fa-sun')
     }    
 }
 const currentTheme = localStorage.getItem('class') ? localStorage.getItem('class') : null;
 
 if (currentTheme) {
     document.body.setAttribute('class', currentTheme);
+    document.getElementById('switch-icon').setAttribute('class', 'fa-solid fa-sun');
 
     if (currentTheme === 'theme--dark') {
         toggleSwitch.checked = true;
+        document.getElementById('switch-icon').setAttribute('class', 'fa-solid fa-moon');
     }
 }
 
